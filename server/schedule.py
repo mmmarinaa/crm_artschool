93e5101201a380ac509353c73719f9daeaea51be
Marina <marina.vyborovas@mail.ru>
2023-11-09 16:19:22 +0800

    Сделана запись уроков в массив

--- server/schedule.py
+++ server/schedule.py
@@ -35,7 +35,7 @@ try:
     cursor.execute("SELECT * FROM training_programs")
     training_programs = cursor.fetchall()
 
-    def generate_schedule(teachers, subjects, rooms, time, days, sullabus, teachers_workload, groups):
+    def generate_schedule(teachers, subjects, rooms, morning_time, evening_time, days, sullabus, teachers_workload, groups):
         schedule = []
 
         for teacher in teachers:
@@ -47,30 +47,46 @@ try:
             cursor.execute(sql_query, (teacher[0],))
             teacher_groups = cursor.fetchall()
 
+            print(teacher_groups)
+
             for group in teacher_groups:
                 sql_query = "SELECT subject_id, hours FROM syllabus WHERE program_id = %s"
                 cursor.execute(sql_query, (group[3],))
                 group_classes = cursor.fetchall()
             
+                i = 0
+                # while int(teacher_workload[0][0]) > 0 and subjects:
+                while i < 1:
+                    subjects = sorted(group_classes, key = lambda x:(x[1]), reverse=True)
+                    
+                    subject = subjects[0] 
+                    available_classrooms = [c for c in rooms if is_available(c, morning_time, schedule)]
+                    if not available_classrooms:
+                        available_classrooms = [c for c in rooms if is_available(c, evening_time, schedule)]   
 
-            while int(teacher_workload[0][0]) > 0 and subjects:
-                subjects = sorted(group_classes, key = lambda x:(x[1]), reverse=True)
-                
-                subject = subjects[0] 
-                available_classrooms = [c for c in rooms if is_available(c, morning_time)]
-                if not available_classrooms:
-                    available_classrooms = [c for c in rooms if is_available(c, evening_time)]   
+                    if not available_classrooms:
+                        break    
+                    
+                    classroom = available_classrooms[0]
 
-                if not available_classrooms:
-                    break    
-                
-                classroom = available_classrooms[0]
+                    if group[5] == 1:
+                        time_slot = random.choice(morning_time)
+                    else:
+                        time_slot = random.choice(evening_time)
+                    
+                    lesson = {
+                        "teacher": teacher[1],
+                        "subject": subject,
+                        "classroom": classroom,
+                        "time_slot": time_slot,
+                    }
+                    schedule.append(lesson)
 
-                # Выбираем случайное время (утро или вечер)
-                if random.choice([True, False]):
-                    time_slot = random.choice(morning_time)
-                else:
-                    time_slot = random.choice(evening_time)
+                    print(lesson)
+
+                    teacher_workload -= group_classes[1]
+                    subjects.remove(subject)
+                    i = 1
 
         return schedule
     
